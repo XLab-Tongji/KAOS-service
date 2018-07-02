ebc134cb190d3e3933252e9e73affec961fe20e8
lidonggg <1225893478@qq.com>
2018-07-02 13:23:16 +0800

    test

--- src/test/java/com/test/controller/RegisterTest.java
+++ src/test/java/com/test/controller/RegisterTest.java
@@ -28,6 +28,10 @@ public class RegisterTest {
     private RegisterService registerService;
     @Before
     public void doInit(){
+        User user1 = userRepository.findByUsername("456789");
+        userRepository.delete(user1);
+        User user2 = userRepository.findByUsername("234567");
+        userRepository.delete(user2);
         userRepository.save(new User("123456","123456"));
         userRepository.save(new User("234567","123456"));
     }
@@ -120,7 +124,9 @@ public class RegisterTest {
 
     @After
     public void doAfter(){
-
-
+        User user1 = userRepository.findByUsername("456789");
+        userRepository.delete(user1);
+        User user2 = userRepository.findByUsername("234567");
+        userRepository.delete(user2);
     }
 }
